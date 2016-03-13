d4479309147600f9a4fe9e3f2c8dc373c0a7ec7e
atakacs91 <takacsandras91@gmail.com>
2016-03-13 20:37:16 +0100

    After Assignment 3

---
+++ RestaurantManager/RestaurantManager.Extensions/RightClickMessageDialogBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Microsoft.Xaml.Interactivity;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+
+namespace RestaurantManager.Extensions
+{
+    public class RightClickMessageDialogBehavior : DependencyObject, IBehavior
+    {
+        public DependencyObject AssociatedObject { get; private set; }
+        
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            this.AssociatedObject = associatedObject;
+            (this.AssociatedObject as Page).RightTapped += Page_RightClick;
+        }
+
+        public void Detach()
+        {
+            (this.AssociatedObject as Page).RightTapped -= Page_RightClick;
+
+        }
+
+        private void Page_RightClick(object sender, RoutedEventArgs e)
+        {
+            new MessageDialog("Thank you for trying out the demo").ShowAsync();
+        }
+    }
+}
